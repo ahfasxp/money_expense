324f924e9cefa67752aa95b17788ca4f4a7ed7df
ahfasxp <ahfas.fazria@gmail.com>
2025-09-17 14:53:27 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:money_expense/app/app.bottomsheets.dart';
+import 'package:money_expense/app/app.dialogs.dart';
+import 'package:money_expense/app/app.locator.dart';
+import 'package:money_expense/app/app.router.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [StackedService.routeObserver],
+    );
+  }
+}
